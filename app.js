adde4c88a40406960d5240d4da21699d180df999
Dimitris Tsikopoulos <dimitristsik@gmail.com>
2020-10-09 16:40:14 +0300

    fix navbar

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
 
- 
-  burger.addEventListener("click", () => {
-    // Toggle Nav
-    nav.classList.toggle("nav-active");
-    // Burger animation
-    burger.classList.toggle("toggle");
-  });
- 
-  // Animate links
-  navLinks.forEach((link, index) => {
-    if (link.style.animation) {
-      link.style.animation = "";
-    } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.6}s`;
-    }
-  });
- 
- 
-     //Animate back on link click
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
+burger.addEventListener("click",function(){
+   nav.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+  
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", function(){
+      if(window.innerWidth>768){
+        nav.classList.remove("nav-active");
+      burger.classList.remove("toggle");
+      }else{
+        nav.classList.toggle("nav-active");
       burger.classList.toggle("toggle");
+      }
+      
     });
   });
-  
- 
 
 
-};
-navSlide();
+// const navSlide = () => {
+//   const burger = document.querySelector(".burger");
+//   const nav = document.querySelector(".nav-links");
+//   const navLinks = document.querySelectorAll(".nav-links li");
 
+//   burger.addEventListener("click", () => {
+//     // Toggle Nav
+//     nav.classList.toggle("nav-active");
+//     // Burger animation
+//     burger.classList.toggle("toggle");
+//   });
+//   // Animate links
+//   navLinks.forEach((link, index) => {
+//     if (link.style.animation) {
+//       link.style.animation = "";
+//     } else {
+//       link.style.animation = `navLinkFade 0.5s ease forwards ${
+//         index / 7 + 0.6
+//       }s`;
+//     }
+//   });
+//   //Animate back on link click
+//   navLinks.forEach((link) => {
+//     link.addEventListener("click", () => {
+//       nav.classList.toggle("nav-active");
+//       burger.classList.toggle("toggle");
+//     });
+//   });
+// };
+// navSlide();
 
 //Typing effect
 let arr = ["Websites", "Applications", "UI/UX Designs"];
