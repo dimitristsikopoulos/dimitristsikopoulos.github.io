970a356509d44bfd08c26b49b4eaa5381a4510e2
Dimitris Tsikopoulos <dimitristsik@gmail.com>
2020-10-09 14:40:13 +0300

    bug fix nav bar 2

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
 
+ 
   burger.addEventListener("click", () => {
     // Toggle Nav
     nav.classList.toggle("nav-active");
     // Burger animation
     burger.classList.toggle("toggle");
   });
+ 
   // Animate links
   navLinks.forEach((link, index) => {
     if (link.style.animation) {
       link.style.animation = "";
     } else {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${
-        index / 7 + 0.6
-      }s`;
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.6}s`;
     }
   });
-  //Animate back on link click
+ 
+ 
+     //Animate back on link click
   navLinks.forEach((link) => {
     link.addEventListener("click", () => {
       nav.classList.toggle("nav-active");
       burger.classList.toggle("toggle");
     });
   });
+  
+ 
+
+
 };
 navSlide();
+
 
 //Typing effect
 let arr = ["Websites", "Applications", "UI/UX Designs"];
